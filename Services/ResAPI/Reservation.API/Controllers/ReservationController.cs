4e2176d79a7886e9f37a76987e0b464af1a89d47
bendenizdeniz <denizcanaltun0@gmail.com>
2022-05-06 16:24:08 +0300

    Insert Files.
    Used 2 sample service and ocelot for gateway .

---
+++ Services/ResAPI/Reservation.API/Controllers/ReservationController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Reservation.API.Infrastructure;
+using Reservation.API.Models;
+using Reservation.API.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reservation.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationController : ControllerBase
+    {
+        private readonly IReservationService reservationService;
+
+        public ReservationController(IReservationService reservationService)
+        {
+            this.reservationService = reservationService;
+        }
+
+        [HttpGet("{Id}")]
+        public ReservationDTO Get(int Id)
+        {
+            return reservationService.GetResByBkgNumber(Id);
+        }
+    }
+}
